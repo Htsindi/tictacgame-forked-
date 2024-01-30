915545e2511f85cbda7c1470cc95de370843585e
Martin Tsindi <matefortechnology@gmail.com>
2024-01-30 13:06:13 +0000

    Sandbox snapshot commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,24 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-export default function Player({name, symbol}){
-  const [isEditing,  setIsEditing] = useState(false);
-
-const btnHandler=()=>{
-  setIsEditing(True);
-  }
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const btnHandler = () => {
+    setIsEditing(True);
+  };
 
   let playerName = <span className="player-name">{name}</span>;
 
-  if (isEditing){
+  if (isEditing) {
     playerName = <input type="text" required />;
-  };
+  }
 
-return(
-  <>
-  <li>
-    <span className='player'>
-       {playerName}
+  return (
+    <li>
+      <span className="player">
+        {playerName}
         <span className="player-symbol">{symbol}</span>
-        <button onClick={btnHandler} >Edit</button>
-    </span>
-  </li>
-  </>
-)
+        <button onClick={btnHandler}>Edit</button>
+      </span>
+    </li>
+  );
 }
