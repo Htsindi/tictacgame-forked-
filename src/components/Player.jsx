29c30147cdb8a50ee130385fef1a66d39178573d
CodeSandbox <support@codesandbox.io>
2024-01-30 10:41:48 +0000

    Sandbox snapshot commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+
+export default function Player({name, symbol}){
+  const [isEditing,  setIsEditing] = useState(false);
+
+const btnHandler=()=>{
+  setIsEditing(True);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing){
+    playerName = <input type="text" required />;
+  };
+
+return(
+  <>
+  <li>
+    <span className='player'>
+       {playerName}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={btnHandler} >Edit</button>
+    </span>
+  </li>
+  </>
+)
+}
